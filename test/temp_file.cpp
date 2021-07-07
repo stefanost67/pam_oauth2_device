2fb8a49947e5707b449b156b6f8658b5b51b0786
jens jensen <j.jensen.ral@outlook.com>
2021-07-07 11:09:51 +0100

    Improved support for named temporaries; content can also be specified as string

--- test/temp_file.cpp
+++ test/temp_file.cpp
@@ -2,6 +2,10 @@
 // Created by jens.jen@stfc.ac.uk on 25/06/2021.
 // Fairly Unix specific, but this was just designed to be a simple RAII file
 //
+// TODO tidy up all the different errors and exceptions
+// TODO handle case where a named file overwrites an existing file
+// TODO check for thread safety
+// TODO allocating FILENAME_MAX for every instance is a bit wasteful...
 
 #include "temp_file.hpp"
 
@@ -15,18 +19,57 @@
 // As it says on the label. Returns true if successful.
 static bool write_data_to_file(FILE *fp, char const *data) noexcept;
 
+// very C-ish
+static void make_temp( char *filename, size_t size, FILE **file )
+{
+    constexpr char const *tempname = "/tmp/pam_oauth2_XXXXXX"; // must have six Xs
+    if(strlen(tempname) >= size)
+	throw "Really can't happen";
+    strncpy(filename, tempname, size);
+    int fd = mkstemp(filename);
+    if(fd < 0)
+	throw "Failed to create temp file";
+    *file = fdopen(fd, "w");  // foo inherits the file descriptor
+    if(!*file)
+	throw "Failed to create file object (out of memory?)";
+}
+
+
+static void save_cwd( char *filename, size_t size )
+{
+    if (!getcwd(filename, size))
+	throw "Insufficient memory"; // can't happen
+}
+
+
+
+static void save_filename(char const *filename, char *dest, size_t size)
+{
+    char *p = dest;
+    if(*filename != '/') {
+	// For a relative path, save the current directory path (in which the relative path is created)
+	// (unless another thread changes it between the getcwd and the file creation?!)
+	save_cwd( dest, size );
+	size_t len = strlen(p);
+	// Add '/' at the end of the path, so we can concatenate the filename
+	dest[len] = '/';
+	dest[++len] = '\0';
+	p += len;
+	size -= len;
+    }
+    if (strlen(filename) >= size)
+	throw "Filename too long";
+    strncpy(p, filename, size);
+}
+
+
+
+
 
 TempFile::TempFile(const char *contents)
 {
-    constexpr char const *tempname = "/tmp/pam_oauth2_XXXXXX";
-    static_assert(strlen(tempname) < max_name_);
-    strncpy(fname_, tempname, max_name_);
-    int fd = mkstemp(fname_);
-    if(fd < 0)
-        throw "Failed to create temp file";
-    FILE *foo = fdopen(fd, "w");  // foo inherits the file descriptor
-    if(!foo)
-        throw "Failed to create file object (out of memory?)";
+    FILE *foo;
+    make_temp(fname_, sizeof(fname_), &foo);
     if(!write_data_to_file(foo, contents)) {
 	fclose(foo);
 	unlink(fname_);
@@ -36,27 +79,42 @@ TempFile::TempFile(const char *contents)
 }
 
 
-/*
+
+// We don't have stringview or filesystem until C++17
 TempFile::TempFile(std::string const &s)
 {
+    FILE *foo;
+    make_temp(fname_, sizeof(fname_), &foo);
+    bool did_it_work_eh = write_data_to_file(foo, s.c_str());
+    fclose(foo);
+    if(!did_it_work_eh) {
+        unlink(fname_);
+        throw "Failed to write data to file";
+    }
 }
-*/
 
 
-/*
+
+
 TempFile::TempFile(char const *filename, std::string const &contents)
 {
-    if(strlen(filename) >= max_name_)
-	throw "Filename too long";
-    strncpy(fname_, filename, max_name_);
+    save_filename(filename, fname_, sizeof(fname_));
+    FILE *foo = fopen(fname_, "w");
+    if(!foo)
+        throw "Failed to open file for writing";
+    bool did_it_work_eh = write_data_to_file(foo, contents.c_str());
+    fclose(foo);
+    if(!did_it_work_eh) {
+	unlink(fname_);
+	throw "Failed to write data to file";
+    }
 }
-*/
+
+
 
 TempFile::TempFile(const char *filename, const char *contents)
 {
-    if(strlen(filename) >= max_name_)
-        throw "Filename too long";
-    strncpy(fname_, filename, max_name_);
+    save_filename(filename, fname_, sizeof(fname_));
     FILE *f = fopen(filename, "w");
     if(!f)
         throw "failed to create file for writing";
@@ -76,20 +134,10 @@ TempFile::~TempFile()
 }
 
 
-std::string TempFile::filename() const
+std::string
+TempFile::filename() const
 {
-    std::string name{fname_};
-    // Temporary names would be full path but not necessarily constructed files
-    if(*name.cbegin() != '/') {
-        char buf[FILENAME_MAX];
-        char const *ret = getcwd(buf, FILENAME_MAX);
-        if(!ret)
-	    throw std::bad_alloc(); // can't happen?
-        std::string cwd{buf};
-        cwd += '/';
-        return cwd+name;
-    }
-    return name;
+    return std::string{fname_};
 }
 
 
@@ -102,6 +150,7 @@ TempFile::dirname() const
         std::string path(fname_, q-p);
         return path;
     }
+    // can't happen: path wasn't set, so return CWD
     char buf[FILENAME_MAX];
     if(!getcwd(buf, FILENAME_MAX))
         throw std::bad_alloc();  // can't happen
--- test/temp_file.hpp
+++ test/temp_file.hpp
@@ -3,28 +3,29 @@
 //
 // This class uses RAII to create a temporary file with a specific content.
 // This is used to create files for testing which are automatically cleared after the test has finished.
+// It really should use boost or something for portable file handling but it needs to minimise dependencies
 
 
 #ifndef __PAM_OAUTH2_DEVICE_TEMP_FILE_HPP
 #define __PAM_OAUTH2_DEVICE_TEMP_FILE_HPP
 
 #include <string>
+#include <cstdio>
 
 /** \brief Create a temporary file with specified content using RAII */
 
 class TempFile {
 private:
-    constexpr static const size_t max_name_ = 24;
-    char fname_[max_name_];
+    char fname_[FILENAME_MAX];
 public:
     /** @brief construct file with given contents */
-//    TempFile(std::string const &contents);
+    TempFile(std::string const &contents);
     /** @brief construct file with given contents */
     TempFile(char const *contents);
     /** @brief construct file with given name and contents */
     TempFile(char const *filename, char const *contents);
     /** @brief construct file with given name and contents */
-//    TempFile(char const *filename, std::string const &contents);
+    TempFile(char const *filename, std::string const &contents);
 
     TempFile(TempFile const &) = delete;
     TempFile(TempFile &&) = delete;
